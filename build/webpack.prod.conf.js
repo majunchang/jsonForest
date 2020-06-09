244cc8842aa6948d7ffc38eca7e02d6deec5e136
majunchang <majunchang@xiaomi.com>
2020-06-09 16:03:31 +0800

    接入sentry

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -32,23 +32,23 @@ const webpackConfig = merge(baseWebpackConfig, {
     new webpack.DefinePlugin({
       'process.env': env
     }),
-    new UglifyJsPlugin({
-      uglifyOptions: {
-        compress: {
-          warnings: false
-        }
-      },
-      sourceMap: config.build.productionSourceMap,
-      parallel: true
-    }),
+    // new UglifyJsPlugin({
+    //   uglifyOptions: {
+    //     compress: {
+    //       warnings: false
+    //     }
+    //   },
+    //   sourceMap: config.build.productionSourceMap,
+    //   parallel: true
+    // }),
     // extract css into its own file
     new ExtractTextPlugin({
       filename: utils.assetsPath('css/[name].[contenthash].css'),
       // Setting the following option to `false` will not extract CSS from codesplit chunks.
       // Their CSS will instead be inserted dynamically with style-loader when the codesplit chunk has been loaded by webpack.
-      // It's currently set to `true` because we are seeing that sourcemaps are included in the codesplit bundle as well when it's `false`, 
+      // It's currently set to `true` because we are seeing that sourcemaps are included in the codesplit bundle as well when it's `false`,
       // increasing file size: https://github.com/vuejs-templates/webpack/issues/1110
-      allChunks: true,
+      allChunks: true
     }),
     // Compress extracted CSS. We are using this plugin so that possible
     // duplicated CSS from different components can be deduped.
@@ -68,8 +68,8 @@ const webpackConfig = merge(baseWebpackConfig, {
         removeComments: true,
         collapseWhitespace: true,
         removeAttributeQuotes: true
-        // more options:
-        // https://github.com/kangax/html-minifier#options-quick-reference
+      // more options:
+      // https://github.com/kangax/html-minifier#options-quick-reference
       },
       // necessary to consistently work with multiple chunks via CommonsChunkPlugin
       chunksSortMode: 'dependency'
@@ -81,7 +81,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     // split vendor js into its own file
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor',
-      minChunks (module) {
+      minChunks(module) {
         // any required modules inside node_modules are extracted to vendor
         return (
           module.resource &&
--- src/views/infiniteScroll.vue
+++ src/views/infiniteScroll.vue
@@ -1,130 +1,133 @@
 <template>
-  <div class="list-view" @scroll="handleScroll">
-    <div
-      class="list-view-phantom"
-      :style="{
-        height: contentHeight
-      }"
-    ></div>
-    <div ref="content" class="list-view-content">
-      <div
-        class="list-view-item"
-        :style="{
-          height: itemHe + 'px'
-        }"
-        v-for="(item, index) in visibleData"
-        :key="index"
-      >
-        {{ item }}
-      </div>
+    <div class="list-view" @scroll="handleScroll">
+        <div
+            class="list-view-phantom"
+            :style="{
+                height: contentHeight
+            }"
+        ></div>
+        <div ref="content" class="list-view-content">
+            <div
+                class="list-view-item"
+                :style="{
+                    height: itemHe + 'px'
+                }"
+                v-for="(item, index) in visibleData"
+                :key="index"
+            >
+                {{ item }}
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 export default {
-  name: "ListView",
-  data() {
-    return {
-      itemHeight: 60,
+    name: 'ListView',
+    data() {
+        return {
+            itemHeight: 60,
 
-      itemHe: 30,
-      dataArr: [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6,
-        7,
-        8,
-        10,
-        1,
-        2,
-        3,
-        4,
-        5,
-        6,
-        7,
-        8,
-        10,
-        1,
-        2,
-        3,
-        4,
-        5,
-        6,
-        7,
-        8,
-        10,
-        1,
-        2,
-        3,
-        4,
-        5,
-        6,
-        7,
-        8,
-        10
-      ],
-      visibleData: [1, 2, 3, 4, 5, 6, 7, 8, 10]
-    };
-  },
-  computed: {
-    contentHeight() {
-      return this.dataArr.length * this.itemHeight + "px";
-    }
-  },
-  mounted() {
-    this.updateVisibleData();
-  },
-  methods: {
-    updateVisibleData(scrollTop) {
-      scrollTop = scrollTop || 0;
-      const visibleCount = Math.ceil(this.$el.clientHeight / this.itemHeight);
-      const start = Math.floor(scrollTop / this.itemHeight);
-      const end = start + visibleCount;
-      this.visibleData = this.dataArr.slice(start, end);
-      this.$refs.content.style.webkitTransform = `translate3d(0, ${start *
-        this.itemHeight}px, 0)`;
+            itemHe: 30,
+            dataArr: [
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                10,
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                10,
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                10,
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                10
+            ],
+            visibleData: [1, 2, 3, 4, 5, 6, 7, 8, 10]
+        };
     },
-    handleScroll() {
-      const scrollTop = this.$el.scrollTop;
-      this.updateVisibleData(scrollTop);
+    computed: {
+        contentHeight() {
+            return this.dataArr.length * this.itemHeight + 'px';
+        }
+    },
+    mounted() {
+        this.updateVisibleData();
+    },
+    methods: {
+        updateVisibleData(scrollTop) {
+            scrollTop = scrollTop || 0;
+            const visibleCount = Math.ceil(
+                this.$el.clientHeight / this.itemHeight
+            );
+            const start = Math.floor(scrollTop / this.itemHeight);
+            const end = start + visibleCount;
+            this.visibleData = this.dataArr.slice(start, end);
+            this.$refs.content.style.webkitTransform = `translate3d(0, ${start *
+                this.itemHeight}px, 0)`;
+        },
+        handleScroll() {
+            this.hello();
+            const scrollTop = this.$el.scrollTop;
+            this.updateVisibleData(scrollTop);
+        }
     }
-  }
 };
 </script>
 
 <style>
 .list-view {
-  height: 400px;
-  overflow: auto;
-  position: relative;
-  border: 1px solid #aaa;
+    height: 400px;
+    overflow: auto;
+    position: relative;
+    border: 1px solid #aaa;
 }
 
 .list-view-phantom {
-  position: absolute;
-  left: 0;
-  top: 0;
-  right: 0;
-  z-index: -1;
+    position: absolute;
+    left: 0;
+    top: 0;
+    right: 0;
+    z-index: -1;
 }
 
 .list-view-content {
-  left: 0;
-  right: 0;
-  top: 0;
-  position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    position: absolute;
 }
 
 .list-view-item {
-  padding: 5px;
-  color: #666;
-  line-height: 30px;
-  margin-top: 30px;
-  border: 1px solid red;
-  box-sizing: border-box;
+    padding: 5px;
+    color: #666;
+    line-height: 30px;
+    margin-top: 30px;
+    border: 1px solid red;
+    box-sizing: border-box;
 }
 </style>
